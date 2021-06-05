4a42a1a5679ea56302942ce2ce7eaf4508f13f82
trushalo <85316448+trushalo@users.noreply.github.com>
2021-06-05 07:37:20 -0500

    Update Ship.js
    Add ship object default. Added crew class.

--- Ship.js
+++ Ship.js
@@ -1,1 +1,32 @@
+//set default ship object
 
+let ship= {
+acceleration: 0,
+cargo: 0,
+charm: 0,
+crew: 0,
+debt: 0,
+handling: 0,
+illegal: 0,
+income: 0,
+inebriation: 0,
+wealth: 0
+
+}
+
+//Crew class
+
+class Crew {
+  constructor(name, acceleration, charm, debt, handling, illegal, income)
+{
+active = this.active;
+name = this.name;
+acceleration = this.acceleration;
+
+charm = this.charm
+debt = this.debt
+handling = this.handling
+illegal = this.illegal
+income = this.income
+}
+}
