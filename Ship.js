b907250d2c4a0e44846c7628b576e03fd5d04810
trushalo <85316448+trushalo@users.noreply.github.com>
2021-06-06 08:30:09 -0500

    Update Ship.js

--- Ship.js
+++ Ship.js
@@ -5,6 +5,7 @@ acceleration: 0,
 cargo: 0,
 charm: 0,
 crew: 0,
+crewCapacity: 2,
 debt: 0,
 handling: 0,
 illegal: 0,
@@ -22,7 +23,6 @@ class Crew {
 active = this.active;
 name = this.name;
 acceleration = this.acceleration;
-
 charm = this.charm
 debt = this.debt
 handling = this.handling
@@ -30,3 +30,7 @@ illegal = this.illegal
 income = this.income
 }
 }
+
+let activeCrew = []
+
+function assignShipStats ()
