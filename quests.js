9d275a873727bf2d4323867645107560d8d7b8a6
trushalo <85316448+trushalo@users.noreply.github.com>
2021-06-06 17:32:08 -0500

    Update quests.js

--- quests.js
+++ quests.js
@@ -1,7 +1,27 @@
+let questArray=[
+
+]
+
+
+
+
+//functions built for use
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random() * (max- min + 1)+ min);
+
+function resultsFromQuestRandom (
+  randVal, greatSuccess, fail, badFail, greatSuccessNode, successNode, failNode, badFailNode
+  ) 
+{
+    if (randVal>greatsuccess) return greatSuccessNode
+    else if (randVal>fail) return successNode
+    else if (randVal>badFail) return failNode
+    else return badFailNode
+}
+
 function pullQuest()
   {
   }
 
-let questArray=[
 
-]
